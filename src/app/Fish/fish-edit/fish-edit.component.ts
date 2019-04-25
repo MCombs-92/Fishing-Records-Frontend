8725ec4b4d66f9a897a4e6b1761ca7ebfd54a23d
Mitchell <mitch.combs.92@gmail.com>
2019-04-25 05:27:55 -0400

    Competed Edit functionality for fish model, styling also completed for the time being

--- src/app/Fish/fish-edit/fish-edit.component.ts
+++ src/app/Fish/fish-edit/fish-edit.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Fish } from '../fish';
+import { FishService } from '../fish.service';
+
 
 @Component({
   selector: 'app-fish-edit',
   templateUrl: './fish-edit.component.html',
   styleUrls: ['./fish-edit.component.css']
 })
+
+
 export class FishEditComponent implements OnInit {
 
-  constructor() { }
+  fish: Fish = new Fish;
+
+  change(): void {
+    this.fshsvc.change(this.fish).subscribe(resp=> {
+      console.log(resp);
+      this.router.navigateByUrl("/fish/list");
+    })
+  }
+
+  constructor(
+    private fshsvc: FishService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    let id = this.route.snapshot.params.id;
+    this.fshsvc.get(id).subscribe(resp=> {
+      this.fish = resp;
+    })
   }
 
 }
