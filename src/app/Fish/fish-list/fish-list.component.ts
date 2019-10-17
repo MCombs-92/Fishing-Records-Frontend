9b98ce7cd6a98dab5963359e868751aa974f843d
Mitchell <mitch.combs.92@gmail.com>
2019-10-17 18:39:14 -0400

    Scaffolded removal code for fish.list, may use in the future

--- src/app/Fish/fish-list/fish-list.component.ts
+++ src/app/Fish/fish-list/fish-list.component.ts
@@ -39,6 +39,23 @@ export class FishListComponent implements OnInit {
       console.log(this.fish);
     })
 
+    // let id = this.route.snapshot.params.id;
+
+    // this.fishsvc.get(id).subscribe(resp => {
+    //   this.f = resp;
+    //   this.f.Markedforremoval = !this.f.Markedforremoval;
+    //   this.fishsvc.change(this.f).subscribe(resp => {
+    //     if(this.f.Markedforremoval) {
+    //       console.log("Record has been marked for removal, please confirm to continue or cancel to unmark")
+    //       this.router.navigateByUrl("/fish/list")
+    //     }
+    //     else {
+    //       console.log("Record is no longer marked for removal")
+    //       this.router.navigateByUrl("/fish/list")
+    //     }
+    //   })
+    // })
+
   }
 
 }
